257781ffeb2512fb841c1d600833310a39775a36
Charles Lovering <cjlovering@wpi.edu>
2018-09-04 21:58:49 -0400

    Removed unncessary inter project docs.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -116,9 +116,6 @@ htmlhelp_basename = 'medoc'
 def setup(app):
   app.add_stylesheet('style.css')
 
-# Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/': None}
-
 # -- Options for todo extension ----------------------------------------------
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
